e240dfffdb2b3419d52be2be2a8951457aecf31b
xJormungandr <richardbarnard101@gmail.com>
2017-06-27 15:45:27 +0200

    Output Added

--- ClassLibrary1/Calculations/Session.cs
+++ ClassLibrary1/Calculations/Session.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,32 @@ namespace ClassLibrary1.Calculations
         static double[] spanLengths;
         public const char Space = ' ';
         public const char Dash = '-';
+        public static Boolean Bool50 = true;
+        public static Boolean Bool115 = true;
+        public static Boolean Bool200 = true;
+        public static FileStream ostrm;
+        public static StreamWriter writer;
+        public static TextWriter oldOut = Console.Out;
+        public static String path = "C:/Users/DenJormungandr/Desktop/VoidconOutput.txt";
 
-        public static void DoLogic()
+
+
+
+
+public static void DoLogic()
         {
+
+            try
+            {
+                ostrm = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open VoidconOutput.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
 
             Profile vp50 = new Profile("VP50", 50, 115, 415, 625, 0.8, 292, 20.55, 6450);
             Profile vp115 = new Profile("VP115", 115, 150, 600, 1250, 0.8, 320, 18.75, 19625);
@@ -58,19 +82,19 @@ namespace ClassLibrary1.Calculations
                 {
                     spanList.Add(s);
                 }
-
+                Console.SetOut(writer);
                 doCalculations(profile,spanList,slabThickness);
+ 
             }
-            
-
-
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
         }
 
         public static void doCalculations(Profile p, List<Double> spanList, double[] slabThickness)
         {
 
             Console.Write(" & & & & ");
-            
 
 
             foreach (Double s in spanList)
@@ -101,6 +125,21 @@ namespace ClassLibrary1.Calculations
                         Beam temp = beamList[(beamList.Count() - 1)];
                         if (printIni)
                         {
+                                if (p.name == "VP50" && Bool50) 
+                                {
+                                    Console.WriteLine(p.name);
+                                    Bool50 = false;
+                                }
+                                else if(p.name == "VP115" && Bool115)
+                                {
+                                    Console.WriteLine(p.name);
+                                    Bool115 = false;
+                                }
+                                else if(p.name == "VP200" && Bool200)
+                                {
+                                    Console.WriteLine(p.name);
+                                    Bool200 = false;
+                                }
                             dataLine.Add((LL / 1000).ToString("F2"));
                             dataLine.Add(new string(Space,2));
                             dataLine.Add(( temp.own_weight / (temp.width / 1000) / 1000).ToString("F3"));
@@ -118,20 +157,29 @@ namespace ClassLibrary1.Calculations
 
                     String temp2 = dataLine[(dataLine.Count() - 1)];                    
                     String newtemp = temp2.Replace("&", "\\\\"); //Geen Fokken Idee Wat Hierdie Doen Nie
-                    dataLine.Add(newtemp);
+                    dataLine.Add(newtemp);                    
                     foreach (var line in dataLine)
                     {
                         Console.Write(line);
                     }
                     Console.WriteLine();
                     dataLine = new List<string>();
+                }                
+                if (t != slabThickness[slabThickness.Count() - 1])
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Next Slab Thickness for " + p.name);
                 }
-                Console.WriteLine("\\midrule");
+                else
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine("Next Voidcon Formwork");
+                    Console.WriteLine(Environment.NewLine);
+                }
             }
 
             Console.WriteLine();
-
-
+                        
             foreach(var b in beamList)
             {
                 String path = "./Output/" + b.name + "_" + b.span + "m" + "_"
@@ -140,8 +188,10 @@ namespace ClassLibrary1.Calculations
                 + ".txt";
                 String out123 = b.printFullRecord();
                 //Util.writeToFile(out123, path);
-            }         
+            }
+
         }
+
     }
 }
 
