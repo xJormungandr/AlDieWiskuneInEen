565e16c0f5941fbaaf482fea5a0ec5153b185cfb
xJormungandr <richardbarnard101@gmail.com>
2017-07-30 16:52:35 +0200

    New Results Class

---
+++ ClassLibrary1/Models/Results.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models
+{
+    class Results
+    {
+        public string profileName;
+        public static List<string> profileLengths = new List<string>();
+
+
+
+    }
+
+    public class ResultsList
+    {
+        public double imposedLoad;
+        public double deadLoad;
+        public double factoredLoad;
+        public double slabThickness;
+    }
+
+}
