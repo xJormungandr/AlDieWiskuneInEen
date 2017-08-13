1b0543c12fc59b3554b43ff774e43c86cb44637b
Poseidon360 <reier360@gmail.com>
2017-08-13 16:24:36 +0200

    Aaa

--- ClassLibrary1/Calculations/Session.cs
+++ ClassLibrary1/Calculations/Session.cs
@@ -3,7 +3,6 @@ using ClassLibrary1.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
